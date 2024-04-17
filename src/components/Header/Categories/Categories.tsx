ddc9b794ad3809f2058010370a23c98313ffa7a5
Elena Demina <lena.demina@hotmail.com>
2024-04-17 14:14:41 +0200

    style(Categories):change css, add transition,bckgr,h1

--- src/components/Header/Categories/Categories.tsx
+++ src/components/Header/Categories/Categories.tsx
@@ -9,16 +9,69 @@ const Categories = () => {
 
   return (
     <div className={styles.categoriesWrapper} data-testid='categories'>
-      <div className={styles.genreBlock} data-testid='genre'></div>
-      <div className={styles.genreBlock} data-testid='genre'></div>
-      <div className={styles.genreBlock} data-testid='genre'></div>
-      <div className={styles.genreBlock} data-testid='genre'></div>
-      <div className={styles.genreBlock} data-testid='genre'></div>
-      <div className={styles.genreBlock} data-testid='genre'></div>
-      <div className={styles.genreBlock} data-testid='genre'></div>
-      <div className={styles.genreBlock} data-testid='genre'></div>
-      <div className={styles.genreBlock} data-testid='genre'></div>
       <Snow />
+      <div className={styles.blocksHolder}>
+        <div
+          className={`${styles.genreBlock} ${styles.block1}`}
+          data-testid='genre'
+        >
+          <h1>Action</h1>
+        </div>
+        <div
+          className={`${styles.genreBlock} ${styles.block2}`}
+          data-testid='genre'
+        >
+          <h1>Adventure</h1>
+        </div>
+        <div
+          className={`${styles.genreBlock} ${styles.block3}`}
+          data-testid='genre'
+        >
+          <h1>Comedy</h1>
+        </div>
+        <div
+          className={`${styles.genreBlock} ${styles.block4}`}
+          data-testid='genre'
+        >
+          <h1>Crime</h1>
+        </div>
+        <div
+          className={`${styles.genreBlock} ${styles.block5}`}
+          data-testid='genre'
+        >
+          <h1>Drama</h1>
+        </div>
+        <div
+          className={`${styles.genreBlock} ${styles.block6}`}
+          data-testid='genre'
+        >
+          <h1>Fantasy</h1>
+        </div>
+        <div
+          className={`${styles.genreBlock} ${styles.block7}`}
+          data-testid='genre'
+        >
+          <h1>Horror</h1>
+        </div>
+        <div
+          className={`${styles.genreBlock} ${styles.block8}`}
+          data-testid='genre'
+        >
+          <h1>Mystery</h1>
+        </div>
+        <div
+          className={`${styles.genreBlock} ${styles.block9}`}
+          data-testid='genre'
+        >
+          <h1>Romance</h1>
+        </div>
+        <div
+          className={`${styles.genreBlock} ${styles.block10}`}
+          data-testid='genre'
+        >
+          <h1>Thriller</h1>
+        </div>
+      </div>
     </div>
   );
 };
